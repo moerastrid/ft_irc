1288cabcede3d5769f6b606e59ac10fbde199564
Thibauld wilhelmus wouter Nuyten <tnuyten@f1r4s2.codam.nl>
2023-10-30 16:46:42 +0100

    improved tester

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -4,16 +4,57 @@
 using std::cout;
 using std::endl;
 
-void test(env& env, string& incoming, int fd) {
+#define COLOR_RED	"\x1b[31m"
+#define COLOR_GREEN	"\x1b[32m"
+
+// Define ANSI escape code to reset text color to the default
+#define COLOR_RESET	"\x1b[0m"
+
+class CustomOutputStream : public std::ostream {
+public:
+	CustomOutputStream(std::ostream& output) : std::ostream(output.rdbuf()), output_stream(output) {}
+
+	template <typename T>
+	CustomOutputStream& operator<<(const T& value) {
+		for(const auto& el : value) {
+			if (el == '\n') {
+				// Special handling for printing the literal "\n"
+				output_stream << "\\n";
+			} else {
+				output_stream << el;
+			}
+		}
+		return *this;
+	};
+
+	// Override the << operator for endl
+	CustomOutputStream& operator<<(std::ostream& (*manipulator)(std::ostream&)) {
+		manipulator(output_stream);
+		return *this;
+	};
+
+private:
+	std::ostream& output_stream;
+};
+
+void test(env& env, string& incoming, int fd, string expected) {
+	CustomOutputStream customOut(std::cout);
 	static Executor ex(env);
 
-	cout << "Processing Incoming message: [" + incoming + "]" << endl;
+	customOut << "Processing Incoming message: [" + incoming + "]" << std::endl;
 
 	Command cmd(incoming, fd);
 
-	cout << "Reply received: " << endl;
+	cout << "Reply received: ";
 	string reply = ex.run(cmd, fd);
-	cout << "[" << reply << "]" << endl;
+
+	if (reply.compare(expected) == 0) {
+		customOut << COLOR_GREEN << "[" << reply << "]" << COLOR_RESET << endl;
+	} else {
+		customOut << COLOR_RED << "[" << reply << "]" << COLOR_RESET << endl;
+	}
+
+
 
 	cout << "===============================" << endl << endl;
 }
@@ -36,18 +77,18 @@ void setup_test(env& env) {
 	string ping2 = "PING localhost\n";
 
 
-	test(env, capreq, fd_user1);
-	test(env, nick1, fd_user1);
-	test(env, user1, fd_user1);
-	test(env, mode1, fd_user1);
-	test(env, ping1, fd_user1);
+	test(env, capreq, fd_user1, "localhost CAP NAK :-\n");
+	test(env, nick1, fd_user1, ":localhost NOTICE neus :Remember to set your username using the USER command.\n");
+	test(env, user1, fd_user1, ":localhost 001 neus :Welcome to Astrid's & Thibauld's IRC server, thibauld!\n");
+	test(env, mode1, fd_user1, ":localhost 403 neus thibauld :No such channel");
+	test(env, ping1, fd_user1, "PONG localhost");
 
 
-	test(env, capreq, fd_user2);
-	test(env, nick2, fd_user2);
-	test(env, user2, fd_user2);
-	test(env, mode2, fd_user2);
-	test(env, ping2, fd_user2);
+	test(env, capreq, fd_user2, "localhost CAP NAK :-\n");
+	test(env, nick2, fd_user2, ":localhost NOTICE astrid :Remember to set your username using the USER command.\n");
+	test(env, user2, fd_user2, ":localhost 001 astrid :Welcome to Astrid's & Thibauld's IRC server, astrid!\n");
+	test(env, mode2, fd_user2, ":localhost 403 astrid astrid :No such channel");
+	test(env, ping2, fd_user2, "PONG localhost");
 
 }
 
@@ -78,29 +119,25 @@ int main() {
 	string message15 = "PART #test :you guys suck\n";
 
 	// WHOIS
-	test(env, message4, fd_user1);
-	test(env, message5, fd_user1);
-	test(env, message6, fd_user1);
+	test(env, message4, fd_user1, ":localhost 311 neus neus thibauld thibauld_PC * :Thibauld WW Nuyten\n");
+	test(env, message5, fd_user1, ":localhost 311 neus neus thibauld thibauld_PC * :Thibauld WW Nuyten\n");
+	test(env, message6, fd_user1, ":localhost 311 neus neus thibauld thibauld_PC * :Thibauld WW Nuyten\n:localhost 311 neus astrid astrid astrid_PC * :astrid geels\n:localhost 401 doofus :No such nickname\n");
 
 	// JOIN test
-	test(env, message7, fd_user1);
-	test(env, message8, fd_user1);
-	test(env, message9, fd_user1);
-	test(env, message10, fd_user1);
+	test(env, message7, fd_user1, ":localhost neus JOIN #test\n:localhost neus JOIN #test2\n:localhost neus JOIN #test3\n:localhost neus JOIN #test4\n");
+	test(env, message8, fd_user1, ":localhost neus JOIN #test3\n:localhost 475 neus #test3 :Bad channel key\n");
+	test(env, message9, fd_user1, ":localhost neus JOIN #test4\n");
+	test(env, message10, fd_user1, "461 ERR_NEEDMOREPARAMS JOIN :Not enough parameters\n");
 
 	// KICK test
-	test(env, message11, fd_user1);
-	test(env, message12, fd_user1);
-	test(env, message13, fd_user1);
-
-	for (const auto& el : env.clients) {
-		cout << el << endl;
-	}
+	test(env, message11, fd_user1, "461 ERR_NEEDMOREPARAMS JOIN :Not enough parameters\n");
+	test(env, message12, fd_user1, ":localhost KICK #test neus :go away\n");
+	test(env, message13, fd_user1, ":localhost 404 #test neus :Cannot kick user from channel they have not joined\n");
 	
 	// PART test
-	test(env, message14, fd_user1);
-	test(env, message14, fd_user2);
-	test(env, message15, fd_user1);
+	test(env, message14, fd_user1, "");
+	test(env, message14, fd_user2, "");
+	test(env, message15, fd_user1, "");
 
 	for (const auto& el : env.clients) {
 		cout << el << endl;
