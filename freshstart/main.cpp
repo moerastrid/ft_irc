2f2add657099f5badb4952dd027abdd620077aa6
Astrid Geels <moerastrid@gmail.com>
2023-11-01 19:13:00 +0100

    message

--- freshstart/main.cpp
+++ freshstart/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char **argv) {
 	
 	Server ircServer(port, argv[2]);
 	// std::cout << "Welcome to our IRC " << ircServer << " \\^.^/" << std::endl;
-	while (1) {
+	// while (1) {
 		ircServer.run();
-	}
+	// }
 	return (EXIT_SUCCESS);
 }
