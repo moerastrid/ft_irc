781f64cf40b99b6e42c52ac557398ccfb8eb6d37
Astrid Geels <moerastrid@gmail.com>
2024-01-14 17:37:35 +0100

    [200~ERR_NICKNAMEINUSE~

--- freshstart/src/Executor.cpp
+++ freshstart/src/Executor.cpp
@@ -248,7 +248,11 @@ string Executor::run_NICK(const vector<string>& args, Client& caller) {
 	// 	return build_notice_reply(caller.getNickname(), caller.getNickname(), "Enter the server password first with PASS to start connection registration");
 
 	if (name_exists(nickname)) { // #TODO Figure out nickcollision vs nicknameinuse
-		return build_reply(ERR_NICKCOLLISION, "NICK", nickname, "Nickname collision KILL from "+ caller.getUsername() + "@" + caller.getHostname());
+		cout << "caller name :" << caller.getNickname() << ":\n";
+		if (caller.getNickname().empty())
+			return build_reply(ERR_NICKCOLLISION, "NICK", nickname, "Nickname collision KILL from "+ caller.getUsername() + "@" + caller.getHostname());
+		else
+			return build_reply(ERR_NICKNAMEINUSE, "NICK", nickname, "Nickname is already in use");
 	}
 	if (nickname.empty() || !verify_name(nickname)) {
 		return build_reply(ERR_ERRONEOUSNICKNAME, "NICK", nickname, "Erroneous nickname");
