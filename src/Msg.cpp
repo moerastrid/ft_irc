b80cd6222fc0174da2b4cf242e939ae5eced8fa4
Astrid Geels <moerastrid@gmail.com>
2024-01-31 14:57:47 +0100

    cleanup message

--- src/Msg.cpp
+++ src/Msg.cpp
@@ -1,31 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Msg.cpp                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ageels <ageels@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/31 14:57:33 by ageels        #+#    #+#                 */
+/*   Updated: 2024/01/31 14:57:34 by ageels        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Msg.hpp"
 
 void	Msg::structure(string text) {
 	cout << "\x1B[35m" << "CLASS-\t\t" << text << "\x1B[0m" << endl;
 }
-
 void	Msg::debug(string text) {
 	cout << "\x1B[36m" << "DEBUG-\t\t" << text << "\x1B[0m" << endl;
 }
-
 void	Msg::info(string text) {
 	cout << "\x1B[34m" << "INFO-\t\t" << text << "\x1B[0m" << endl;
 }
-
 void	Msg::warning(string text) {
 	cout << "\x1B[33m" << "WARNING-\t" << text << "\x1B[0m" << endl;
 }
-
 void	Msg::error(string text) {
 	cout << "\x1B[31m" << "ERROR-\t\t" << text << "\x1B[0m" << endl;
 }
 
-Msg::Msg() {}								//default constructor
-
+Msg::Msg() {}
 Msg::Msg(const Msg &src) {
 	*this = src;
 }
-
 Msg	&Msg::operator=(const Msg &src) {
 	(void)src;
 	return (*this);
@@ -34,7 +40,6 @@ Msg	&Msg::operator=(const Msg &src) {
 typedef void (Msg::*Msgptr)(void);
 
 Msg::~Msg() { }
-
 Msg::Msg(string text, string level) {
 	void	(Msg::*Msgptr[])(string) = {&Msg::structure, &Msg::debug, &Msg::info, &Msg::warning, &Msg::error};
 	string severity[] = {"CLASS", "DEBUG", "INFO", "WARNING", "ERROR"};
